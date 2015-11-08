033a18a2c8f5d057e2bd81d97d4207a86f0d6ba0
Alisson Carvalho <webalissoncs@gmail.com>
2015-11-07 22:55:28 -0200

    Ajustes no angular

--- web/js/controller.js
+++ web/js/controller.js
@@ -7,6 +7,8 @@ function($scope, $global, $http, PlacesApi){
   $scope.place = null;
   $scope.places = {};
   $scope.loading = false;
+  $scope.placeStatement = "create";
+  $scope.search = {$:""};
 
   PlacesApi.get(function(r){
     $scope.places = r.data;
@@ -20,16 +22,32 @@ function($scope, $global, $http, PlacesApi){
   });
 
 
-  $scope.edit = function(place){
+  $scope.openEdit = function(place){
     $scope.place = place;
+    $scope.placeStatement = "edit";
+  };
+  $scope.openCreate = function(place){
     console.log(place);
+    $scope.place = null;
+    $scope.placeStatement = "create";
   };
 
   $scope.delete = function(place){
 
+    if(!confirm("Tem certeza que deseja excluir " + place.name + "?")) {
+      return;
+    }
+
+    $scope.loading = true;
     PlacesApi.delete(place.id, function(r){
+
       $scope.places.splice(place, 1);
+
+      $scope.loading = false;
+
     }, function(r){
+
+      $scope.loading = false;
 
     });
 
@@ -37,8 +55,19 @@ function($scope, $global, $http, PlacesApi){
 
   $scope.save = function(place){
 
+    $scope.loading = true;
     PlacesApi.save(place, function(r){
+      $scope.loading = false;
+
     }, function(r){
+      $scope.loading = false;
+
+      console.log(r);
+
+      if(r.status == 422) {
+        alert("Erro de validação. Verifique os dados");
+      }
+
     });
 
   };
