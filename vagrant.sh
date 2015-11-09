2d8c9aa7e4f0117ddd29aef6d1091b61cf299084
Alisson Carvalho <webalissoncs@gmail.com>
2015-11-09 13:15:48 -0200

    Adicionando e ajustando Vagrantsh

--- vagrant.sh
+++ vagrant.sh
@@ -13,6 +13,9 @@ echo 'Installed Vim'
 #curl
 sudo apt-get install -y curl
 
+#git 
+sudo apt-get install -y git
+
 #configuracoes locale
 sudo locale-gen en_US en_US.UTF-8 hu_HU hu_HU.UTF-8
 sudo dpkg-reconfigure locales
@@ -70,37 +73,23 @@ echo 'server {
   sendfile  off;
 
   location / {
-        # try to serve file directly, fallback to front controller
         try_files $uri /index.php$is_args$args;
         expires 0;
     }
-
-    # If you have 2 front controllers for dev|prod use the following line instead
-    # location ~ ^/(index|index_dev)\.php(/|$) {
     location ~ ^/index\.php(/|$) {
-        # the ubuntu default
         fastcgi_pass   unix:/var/run/php5-fpm.sock;
-        # for running on centos
-        #fastcgi_pass   unix:/var/run/php-fpm/www.sock;
 
         fastcgi_split_path_info ^(.+\.php)(/.*)$;
         include fastcgi_params;
         fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
         fastcgi_param HTTPS off;
-
-        # Prevents URIs that include the front controller. This will 404:
-        # http://domain.tld/index.php/some-path
-        # Enable the internal directive to disable URIs like this
-        # internal;
     }
-
-    #return 404 for all php files as we do have a front controller
     location ~ \.php$ {
         return 404;
     }
 
-    error_log /var/log/nginx/project_error.log;
-    access_log /var/log/nginx/project_access.log;
+    # error_log /var/log/nginx/project_error.log;
+    # access_log /var/log/nginx/project_access.log;
 
 }
 ' > tmp.conf
