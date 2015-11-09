94fd22f3c5b4b3b36ae0b43ab89818a75381f41c
Alisson Carvalho <webalissoncs@gmail.com>
2015-11-09 00:17:41 -0200

    Package node

--- web/Gruntfile.js
+++ web/Gruntfile.js
@@ -1,5 +1,34 @@
 module.exports = function(grunt){
 	
+grunt.initConfig({
+
+uglify: {
+	javascript: {
+		options: {},
+		files: {
+			'dist/output.min.js': [
+				'js/app.js',
+				'js/controller.js',
+				'js/service.js',
+				'js/custom.js'
+			]
+		}
+	},
+	css: {
+		options: {},
+		files: {
+			'dist/output.min.css': [
+				'css/custom.css'
+			]
+		}
+	},
+}
+
+});
+
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+
+	grunt.registerTask('default', ['uglify']);
 
 };
