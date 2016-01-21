ec77100718bac0dcf88b2125d221615480337dbc
Alisson Carvalho <webalissoncs@gmail.com>
2016-01-21 20:20:39 -0200

    endereço da api

--- web/js/app.js
+++ web/js/app.js
@@ -2,8 +2,8 @@ var dmap = angular.module('dmap', ['angular.chosen']);
 
 dmap.constant('global', {
   url: function(p){
-  	return 'http://localhost:4058/api' + p;
-    //return 'http://93.188.167.142/api' + p;
+  	//return 'http://localhost:4058/api' + p;
+   return 'http://descartemap.com.br/api' + p;
   },
   mapsApi: 'AIzaSyBMc2jGxgZ4LV-HTuU_m2ljhuYINIIVx3w'
 });
